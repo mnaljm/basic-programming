a2dd3528018c5a5a3081590e7a2a2430ef8bfcd9
Magnus Naasade <magnaa01@herningsholm.dk>
2025-06-23 13:59:16 +0200

    Add flower comparison script
    Introduces a script that prompts the user for their favorite flower and responds with specific messages based on the input, handling case sensitivity for 'Spathiphyllum'.

---
+++ 3.1.10_compare.py
@@ -0,0 +1,7 @@
+input = str(input("enter your favourite flower: "))
+if input == "Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!")
+elif input == "spathiphyllum":
+    print("No, I want a big Spathiphyllum!")
+else:
+    print(f"Spathiphyllum! Not {input}!")
