c129bbe080ea2d6935669d13a95d5b88bfbce355
Magnus Naasade <magnaa01@herningsholm.dk>
2025-06-23 12:36:09 +0200

    Opgave 2.5.3
    Introduces a Python script that calculates the number of seconds in a specified number of hours, with explanatory comments throughout.

---
+++ 2.5.3_comments.py
@@ -0,0 +1,10 @@
+#this program computes the number of seconds in a given number of hours
+# this program has been written two days ago
+
+a = 2 # number of hours
+seconds = 3600 # number of seconds in 1 hour
+
+print("Hours: ", a) #printing the number of hours
+print("Seconds in Hours: ", a * seconds) # printing the number of seconds in a given number of hours
+
+#this is the end of the program that computes the number of seconds in a given number of hours.
