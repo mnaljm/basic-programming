fa68b8bb4a9ca2e03157a334407c28ad7db0c552
Magnus Naasade <magnaa01@herningsholm.dk>
2025-06-23 12:18:41 +0200

    Hej morten

---
+++ 2.4.10_operators.py
@@ -0,0 +1,20 @@
+x =int(0)
+x = float(x)
+# Udtrykket 3x3 - 2x2 + 3x - 1 betyder:
+# 3 * x^3  (3 gange x i tredje potens)
+# minus 2 * x^2  (minus 2 gange x i anden potens)
+# plus 3 * x  (plus 3 gange x)
+# minus 1  (minus 1)
+# I Python skrives potens som **, så x^3 bliver x**3 og x^2 bliver x**2.
+y = 3*x**3 - 2*x**2 + 3*x - 1
+print("y =", y)
+
+x = int(1)
+x = float(x)
+y = 3*x**3 - 2*x**2 + 3*x - 1
+print("y =", y)
+
+x =int(-1)
+x = float(x)
+y = 3*x**3 - 2*x**2 + 3*x - 1
+print("y =", y)
